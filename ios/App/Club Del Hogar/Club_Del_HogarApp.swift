6e30fe53131dcfd68a984c9ee2bddecdb294cc24
Alan79238 <180443605+Alan79238@users.noreply.github.com>
2025-11-03 11:01:54 -0700

    Cambios para iOS

---
+++ ios/App/Club Del Hogar/Club_Del_HogarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Club_Del_HogarApp.swift
+//  Club Del Hogar
+//
+//  Created by Sistemas Perseo on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Club_Del_HogarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/App/Club Del HogarTests/Club_Del_HogarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Club_Del_HogarTests.swift
+//  Club Del HogarTests
+//
+//  Created by Sistemas Perseo on 03/11/25.
+//
+
+import Testing
+@testable import Club_Del_Hogar
+
+struct Club_Del_HogarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
